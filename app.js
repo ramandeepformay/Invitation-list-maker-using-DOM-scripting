f3cc89b314ae0a4bcbafa643f2037ad5598b58a6
ramandeepformay <ramandeepformay@gmail.com>
2019-08-07 03:47:30 -0400

    checked invitation functionality is added

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 //capturing all the elements
 const form = document.querySelector(".registrar");
 const input = document.querySelector("input");
+const main = document.querySelector(".wrapper");
 const ul =document.getElementById("invite");
+//create elements for div label and append those to make amn element which contains checkbox
+const div = document.createElement("div");
+const label = document.createElement("label");
+const inputFilter = document.createElement("input");
+inputFilter.type = "checkbox";
+label.textContent ="Hide those who haven't responed";
+label.appendChild(inputFilter);
+div.appendChild(label);
+main.insertBefore(div, ul);
+
 
 //applying event listener
 form.addEventListener("submit", (e)=>{
@@ -94,7 +105,34 @@ ul.addEventListener("click", (e)=>{
         li.removeChild(input);
         target.textContent = "Edit";
     } 
-    
-    }
+  }
+})
 
+//Event listener on hide elemnts to check who are invited or who are not
+inputFilter.addEventListener("change",(e)=>{
+    //storing bollean value of checked or not in check
+    const check =e.target.checked;
+    //storing all the ul children in list
+    const list = ul.children;
+    if(check){
+        //looping through all the li elements
+        for(let i=0; i<list.length; i++){
+            //storing all the values in li
+           let li = list[i]
+           //if li contains class outline then display it otherwise block it
+            if(li.className === "outline"){
+                li.style.display = "block";
+            }
+            else{
+                li.style.display = "none";
+            }
+        }
+    }
+    else{
+        for(let i=0; i<list.length; i++){
+           let li =list[i];
+            li.style.display = "block";
+        }   
+
+    }
 })
